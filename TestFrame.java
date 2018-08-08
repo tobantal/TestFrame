6e9cbf5acaa8a34499545aa90bafdb8817d32f27
Anton Tobolkin <tobantal@gmail.com>
2018-08-08 14:16:26 +0700

    simple TestFrame class

---
+++ TestFrame.java
@@ -0,0 +1,23 @@
+package testframe;
+
+import java.awt.*;
+
+class TestFrame extends Frame
+{
+    public TestFrame()
+    {
+        super();
+        setSize( 400, 400 );
+        setLayout( new BorderLayout() );
+        add( new Button( "North" ), BorderLayout.NORTH );
+        add( new Button( "South" ), BorderLayout.SOUTH );
+        add( new Button( "East" ), BorderLayout.EAST );
+        add( new Button( "West" ), BorderLayout.WEST );
+        add( new Button( "Center" ), BorderLayout.CENTER );
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TestFrame();
+    }
+}
