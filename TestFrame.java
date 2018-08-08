14746e922b5fa1f47e1b18144f17080552f1d98d
Anton Tobolkin <tobantal@gmail.com>
2018-08-08 14:38:57 +0700

    Combining multiple layouts

--- TestFrame.java
+++ TestFrame.java
@@ -8,42 +8,28 @@ class TestFrame extends Frame
     {
         super();
 
-        // Create the grid bag layout manager instance and
-        // an associated constraints object
-        GridBagLayout gridBag = new GridBagLayout();
-        GridBagConstraints gridConstraints = new GridBagConstraints();
-
-        setLayout( gridBag );
+        setLayout( new BorderLayout() );
         setBackground( Color.lightGray );
-        // Create some buttons, adding them to the grid bag
-        // using three equal portions
-        gridConstraints.fill = GridBagConstraints.BOTH;
-        gridConstraints.weightx = 1.0;
-        createButton( "Button1", gridBag, gridConstraints );
-        createButton( "Button2", gridBag, gridConstraints );
-        createButton( "Button3", gridBag, gridConstraints );
-        // The fourth button on this line gets the space that’s left
-        gridConstraints.gridwidth =
-                GridBagConstraints.REMAINDER;
-        createButton( "Button4", gridBag, gridConstraints );
-        // Create another button on the next line that uses
-        // the entire width.
-        gridConstraints.weightx = 0.0;
-        createButton( "Button5", gridBag, gridConstraints );
-        gridConstraints.gridwidth = GridBagConstraints.RELATIVE;
+
+        // Create a subpanel for the toolbar
+        Panel toolbar = new Panel();
+        toolbar.setLayout( new FlowLayout() );
+        toolbar.add( new Button( "Button 1" ) );
+        toolbar.add( new Button( "Button 2" ) );
+        toolbar.add( new Button( "Button 3" ) );
+        toolbar.add( new Button( "Button 4" ) );
+        toolbar.add( new Button( "Button 5" ) );
+
+        // Add the toolbar to the main panel
+        add( toolbar, BorderLayout.NORTH );
+
+        // Add a multi-line editor to the reset of the panel
+        add( new TextArea("text area"), BorderLayout.CENTER );
 
         setSize( 400, 400 );
         setVisible(true);
     }
 
-    protected void createButton( String sName,
-                                 GridBagLayout gridBag,
-                                 GridBagConstraints gridConstraint )
-    {
-        Button button = new Button( sName );
-        gridBag.setConstraints( button, gridConstraint );
-        add( button );
-    }
 
     public static void main(String[] args) {
         new TestFrame();
